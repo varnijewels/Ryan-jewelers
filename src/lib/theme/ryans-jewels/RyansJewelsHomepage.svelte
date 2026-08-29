359acfea3cf110eda39790f26d42e1f14ab1271f
Sujal Amreliya <fig3.varni@gmail.com>
2026-08-29 11:23:07 +0530

    fix: keep homepage sections immediately paintable

--- src/lib/theme/ryans-jewels/RyansJewelsHomepage.svelte
+++ src/lib/theme/ryans-jewels/RyansJewelsHomepage.svelte
@@ -129,11 +129,6 @@
 		background: #fff;
 	}
 
-	.rj-home > :global(:not(:first-child)) {
-		content-visibility: auto;
-		contain-intrinsic-block-size: auto 700px;
-	}
-
 	.rj-hero-wrap {
 		position: relative;
 		width: 100%;
